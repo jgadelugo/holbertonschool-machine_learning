4d168765f638f1e15564e1c7d1cf881e96583e4a
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-04-16 13:51:58 -0400

    [TASK] Function that returns the transpose of a 2D Matrix

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function that returns the transpose of a 2D matrix"""
+
+
+def matrix_transpose(matrix):
+    """function that returns the transpose of a 2D matrix"""
+    new_matrix = []
+    for i in range(len(matrix[0])):
+        new_matrix.append([])
+        for x in range(len(matrix)):
+                new_matrix[i].append(matrix[x][i])
+    return new_matrix
