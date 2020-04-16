84e09a9469bf8274c526cf3c40c63f1cadcb93de
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-04-16 19:16:56 -0400

    [TASK] multiply matrices using numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    concatenate two matrices along a specific axis
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Function that concatenates two matrices along a specific axis
+    """
+    return np.matmul(mat1, mat2)
