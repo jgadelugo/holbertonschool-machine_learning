7204e6ff1a02070bb79fca861e09b59df5bcc11d
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-04-16 18:58:59 -0400

    [TASK] Add, subtract, multiply, and divide matrices

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    performs element-wise
+    addition, subtraction, multiplication and division
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Function that performs element-wise
+    addition, subtraction, multiplication and division
+     """
+    return ((mat1 + mat2), (mat1 - mat2), (mat1 * mat2), (mat1 / mat2))
