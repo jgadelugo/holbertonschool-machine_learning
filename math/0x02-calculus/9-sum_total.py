c77f6a90115076ec9490964032768ccfc1625972
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-04-25 15:51:09 -0400

    Change to do it recursively

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -2,8 +2,11 @@
 """ Function that calculates Sum (i=1, n) i^2"""
 
 
-def summation_i_squared(n):
-    total = 0
-    for x in range(1, n + 1):
-        total += x ** 2
-    return total
+def summation_i_squared(n, total=0):
+    try:
+        if n > 0:
+            return summation_i_squared(n - 1, total + n ** 2)
+        return total
+    except:
+        return None
+
