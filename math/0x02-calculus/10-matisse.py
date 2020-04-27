f3982873f5d91b3ae5825961a8248ade34c79a5e
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-04-26 22:34:16 -0400

    [BUG FIX] Add edge case

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,7 +4,11 @@
 
 def poly_derivative(poly):
     """calculates the derivative of a polynomial """
+    if isinstance(poly, list) is False or len(poly) == 0:
+        return None
     size = len(poly)
+    if size == 0:
+        return [0]
     for x in range(size):
         poly[x] *= x
     return poly[1:]
--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n, total=0):
     """ Function that calculates Sum (i=1, n) i^2"""
-    if n > 0:
-        return summation_i_squared(n - 1, total + n ** 2)
-    return total
+    if isinstance(n, int) is False or n < 1:
+        return None
+    return int((n * (n + 1) / 2) * (2 * n + 1) / 3)
