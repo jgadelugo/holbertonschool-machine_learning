20780f35e2ef209e2a40d0c2af66a9ba8bf67c20
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-04-26 22:56:51 -0400

    [TASK] Calculate the integral of a polynomial

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -1,11 +1,12 @@
-#!usr/bin/env/env python
+#!/usr/bin/env python3
 """ Function that calculates the derivative of a polynomial """
 
 
 def poly_integral(poly, c=0):
-    """calculates the derivative of a polynomial """
+    """calculates the derivative of a polynomial .is_integer()"""
 
     size = len(poly)
-    for x in range(1, size):
-        poly[x - 1] /= x
+    for x in range(1, size + 1):
+        v = poly[x - 1] / x
+        poly[x - 1] = int(v) if (v).is_integer() else v
     return [c] + poly
