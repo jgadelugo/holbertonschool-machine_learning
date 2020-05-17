cb7c09755e7125eaf157bb2242bf67cb211a96b7
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-17 14:58:28 -0400

    Create file

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""builds, trains, and saves a neural network classifier"""
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """Evaluates the output of a neural network
+    @X: numpy.ndarray - input data to evaluate
+    @Y: numpy.ndarray - containing the one-hot labels for X
+    @save_path: location to load the model from
+    * not allowed to use tf.saved_model
+    Returns: the network’s prediction, accuracy, and loss, respectively
+    """
+    pass
