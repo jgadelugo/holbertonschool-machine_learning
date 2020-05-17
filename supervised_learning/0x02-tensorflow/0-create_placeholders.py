37dfcbba3dc47b05a6790d2b720cc8899b7d1b96
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-17 13:05:51 -0400

    [TASK] Create two placeholders x, y for the neural network

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """ Function returns two placeholders, x and y, for the neural network """
+    x = tf.placeholder(tf.float32, [None, nx], name="x")
+    y = tf.placeholder(tf.float32, [None, classes], name="y")
+    return (x, y)
