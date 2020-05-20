2763ca9d9e9dd6ea9f3656fe0aeaddcc6c81aa31
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-20 15:18:54 -0400

    Converts a numeric label into a one-hot matrix

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
+""" Creates one-hot matrix """
+import numpy as np
 
 
 def one_hot_encode(Y, classes):
-    """  """
+    """Converts a numeric label vector into a one-hot matrix
+    @Y: numpy.ndarray w/ shape (m,) - contains numeric class labels
+        @m: is the number of examples
+    @classes: is the maximum number of classes found in Y
+    Returns: one-hot encoding of Y with shape(classes, m) or None if fails
+    """
+    if type(Y) is not np.ndarray:
+        return None
+    size = len(Y)
+    if size == 0:
+        return None
+    if type(classes) is not int or classes <= Y.max():
+        return None
+
+    new = np.zeros((classes, size))
+    for i in range(size):
+        new[Y[i]][i] = 1
+    return new
