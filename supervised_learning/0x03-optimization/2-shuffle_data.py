ac4040a9d85278034aa9dec9f9eb2a6ddcf89fb7
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-23 20:32:35 -0400

    [TASK] Shuffle the data points in two matrices the same way

---
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Shuffle X and Y to help against over fitting"""
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """ Shuffles X and Y to help against over fitting
+    @X: numpy.ndarray of shape (m, nx) to shuffle
+        @m: number of data points
+        @nx: number of features in X
+    @Y: numpy.ndarray shape (m, ny) to shuffle
+        @m: Same number of data points in X
+        @ny: number of features in Y
+    Returns: the shuffled X and Y matrices
+    """
+    X = np.random.permutation(X)
+    Y = np.random.permutation(Y)
+    return (X, Y)
