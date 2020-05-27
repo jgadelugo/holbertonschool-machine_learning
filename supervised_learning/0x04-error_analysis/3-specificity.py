b4b6e723ff0869a569145d4fe26e9fda7c1a194c
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-27 12:45:54 -0400

    [TASK] Calculates the specificity for eash class in a confusion matrix

---
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" calculates the specificity for each class in a confusion matrix"""
+import numpy as np
+
+
+def precision(confusion):
+    """calculates the specificity for each class in a confusion matrix
+    @confusion: confusion np.ndarray - shape (classes, classes) -
+    rows represent correct labels, columns represent predicted labels
+        @classes: number of classes
+    Return: np.ndarray - shape (classes,) - specificity of each class
+    """
+    true_pos = confusion.diagonal()
+
+    false_neg = confusion.sum(axis=1) - true_pos
+    false_pos = confusion.sum(axis=0) - true_pos
+
+    true_neg = confusion.sum() - (true_pos + false_neg + false_pos)
+
+    return true_neg / (true_neg + false_pos)
