ff75fb2d035419b176b1b0b8be833bd555223023
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-27 11:38:47 -0400

    [TASK] Create a confusion matrix

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+""" Function that creates a confusion matrix """
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """ Creates a confusion matrix
+    @labels: one-hot np.ndarray - shape (m, classes) - correct
+    labels for each data point
+        @m: number of data points
+        @classes: number of classes
+    @logits:  one-hot np.ndarray - shape (m, classes) - predicted labels
+    Return: confusion np.ndarray - shape (classes, classes) - with row
+    representing the correct labels and columns representing
+    predicted labels
+    """
+    return np.matmul(labels.T, logits)
