2325a38785dd9bd4b0cc1980e2d88be70d1fcf49
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-28 19:31:57 -0400

    [TASK] neural network using batch normalization

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -4,7 +4,7 @@ using batch normalization """
 import numpy as np
 
 
-def batch_norm(Z, gamma, beta, epsilon):
+def batch_norm(Z, gamma, beta, eps):
     """ Normalize an unactivated output of a neural network
     using batch normalization
     @Z: numpy.ndarray - shape (m, n) should be normalized
@@ -14,8 +14,8 @@ def batch_norm(Z, gamma, beta, epsilon):
             for batch normalization
     @beta: numpy.ndarray - shape(1, n) offsets used
             for batch normalization
-    @epsilon: small number used to avoid division by zero
+    @eps: small number used to avoid division by zero
     Return: normalized Z matrix
     """
-    z_normalize = (Z - np.mean(Z, axis=0)) / np.sqrt(np.var(Z, axis=0) + epsilon)
+    z_normalize = (Z - np.mean(Z, axis=0)) / np.sqrt(np.var(Z, axis=0) + eps)
     return np.multiply(gamma, z_normalize) + beta
