61e4c1ac73e7f9aaaa1f212dce97debd140e2bd4
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-29 17:08:38 -0400

    [TASK] Fix pycodestyle

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 def l2_reg_cost(cost):
     """calculates the cost of a neural network with L2 regularization
     @cost: tensor containing the cost of the network without L2 regularization
-    Return:  tensor containing the cost of the network accounting for L2 regularization
+    Return:  tensor containing the cost of the network accounting
+    for L2 regularization
     """
     return cost + tf.losses.get_regularization_losses()
