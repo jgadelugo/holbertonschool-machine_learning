2a26b9f0d45770d2b2816f8b000ac5bc8504e4d0
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-06-02 12:07:19 -0400

    [TASK] Add comments and break down to easier understand

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -3,25 +3,30 @@
 import tensorflow.keras as K
 
 
-def build_model(nx, layers, activations, lamb, keep_prob):
+def build_model(nx, layers, activations, lambtha, keep_prob):
     """builds a neural network with keras
     @nx: number of input features to the network
     @layers: list of the number of nodes in each layer
     @activations: list of activation functions used for each layer
-    @lamb: L2 regularization parameter
+    @lambtha: L2 regularization parameter
     @keep_prob: probability that a node will be kept for dropout
     Return: keras model
     """
-    # create a regularizer that applies an L2 regularization penalty
+    # applies an L2 regularization penalty
+    reg = K.regularizers.l2(lambtha)
 
+    # first densely-connected layer
     lay = K.layers.Dense(layers[0], input_shape=(nx,),
                          activation=activations[0],
-                         kernel_regularizer=K.regularizers.l2(lamb))
+                         kernel_regularizer=reg)
 
-    model = K.Sequential([lay])
+    # create model and add first layer
+    model = K.Sequential()
+    model.add(lay)
 
+    # add remaining densely-connected layers
     for layer, activ in zip(layers[1:], activations[1:]):
         model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(layer, activation=activ,
-                                 kernel_regularizer=K.regularizers.l2(lamb)))
+                                 kernel_regularizer=reg))
     return model
