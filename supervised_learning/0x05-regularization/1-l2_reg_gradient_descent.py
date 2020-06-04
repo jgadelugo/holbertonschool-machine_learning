438712a14863eddadd3f11329add70ce1fa459a9
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-06-04 10:06:15 -0400

    [TASK] Update weights and biases of a neural network using gradient descent with L2 regularization

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """updates the weights and biases of a neural network using gradient
 descent with L2 regularization """
+import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
@@ -17,4 +18,20 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     @lambtha: L2 regularization parameter
     @L: number of layers of the network
     """
-    pass
+    w = weights
+    m = Y.shape[1]
+
+    for i in range(L, 0, -1):
+        if i == L:
+            dZ = cache['A' + str(i)] - Y
+        else:
+            dZ1 = np.matmul(w['W' + str(i + 1)].T, dZ)
+            dZ = (1 - cache['A' + str(i)] ** 2) * dZ1
+
+        dW = np.matmul(dZ, cache["A" + str(i - 1)].T) / m
+
+        reg = dW + (lambtha / m) * w['W' + str(i)]
+        db = np.sum(dZ, axis=1, keepdims=True) / m
+
+        weights['W' + str(i)] = w['W' + str(i)] - (reg * alpha)
+        weights['b' + str(i)] = w['b' + str(i)] - (db * alpha)
