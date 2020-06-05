8a52e9e0b31651c5f033574619845053e82d2751
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-06-04 21:32:22 -0400

    [TASK] update weights and biases

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -24,11 +24,11 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     for i in range(L, 0, -1):
         if i == L:
             dZ = cache['A' + str(i)] - Y
+            dW = np.matmul(dZ, cache["A" + str(i - 1)].T) / m
         else:
             dZ1 = np.matmul(w['W' + str(i + 1)].T, dZ)
             dZ = (1 - cache['A' + str(i)] ** 2) * dZ1
-
-        dW = np.matmul(dZ, cache["A" + str(i - 1)].T) / m
+            dW = np.matmul(dZ, cache["A" + str(i - 1)].T) / m
 
         reg = dW + (lambtha / m) * w['W' + str(i)]
         db = np.sum(dZ, axis=1, keepdims=True) / m
