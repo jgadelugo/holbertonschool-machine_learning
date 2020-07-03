96440121ac6c13750cf22cb07d40cd347b4ef497
Jose Alvarez de Lugo <josealvarezdelugo@MacBook-Pro.local>
2020-07-03 16:52:32 -0400

    [TASK] Process outputs for Yolo v3 algo

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -105,8 +105,8 @@ class Yolo:
             # normalize
             bx = bx / gw
             by = by / gh
-            bw = bw / self.model.input.shape[1]
-            bh = bh / self.model.input.shape[2]
+            bw = bw / self.model.input.shape[1].value
+            bh = bh / self.model.input.shape[2].value
 
             x1 = (bx - bw / 2) * image_width
             y1 = (by - bh / 2) * image_height
