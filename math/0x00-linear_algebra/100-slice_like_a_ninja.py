f47b5f60600874a40e09ff25049ef3635efdd54f
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-07-08 18:05:04 -0400

    [TASK] remove unused import of numpy

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -2,11 +2,13 @@
 """
     concatenate two matrices along a specific axis
 """
-import numpy as np
 
 
-def np_cat(mat1, mat2, axis=0):
+def np_slice(matrix, axes={}):
     """
-    Function that concatenates two matrices along a specific axis
+    that slices a matrix along a specific axes
     """
-    return np.concatenate((mat1, mat2), axis)
+    slicer = [slice(None)] * len(matrix.shape)
+    for key in axes.keys():
+        slicer[key] = slice(*axes[key])
+    return matrix[tuple(slicer)]
