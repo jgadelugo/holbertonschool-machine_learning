ed4e3813b76153fa997c7e0666868d203ffef14b
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-07-23 20:40:22 -0400

    [TASK] Get the determinant of a matrix recursively

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,36 @@
+#!usr/bin/env python3
+
+
+def determinant(matrix, total=0):
+    """Calculates the determinant of a matrix
+    @matrix: list of lists whose determinant should be calculated
+    Return: the determinant of matrix
+    """
+    if not isinstance(matrix, list) or matrix == [] or not isinstance(matrix[0], list):
+        raise TypeError('matrix must be a list of lists')
+
+    size = len(matrix)
+
+    if size == 1 and len(matrix[0]) == 0:
+        return 1
+
+    for l in matrix:
+        if not isinstance(l, list):
+            raise TypeError('matrix must be a list of lists')
+        if len(l) != size:
+            raise ValueError('matrix must be a square matrix')
+
+    if size == 1:
+        return matrix[0][0]
+    if size  == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+
+    for i in range(size):
+        sub_matrix = matrix[1:]
+
+        for j in range(len(sub_matrix)):
+            sub_matrix[j] = sub_matrix[j][0:i] + sub_matrix[j][i+1:]
+        sign = (-1) ** (i % 2)
+        sub_det = determinant(sub_matrix)
+        total += sign * matrix[0][i] * sub_det
+    return total
