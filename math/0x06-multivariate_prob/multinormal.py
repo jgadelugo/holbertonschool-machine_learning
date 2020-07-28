cd12b40b5b3e8b909b84ae56b44348f0d6d330d3
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-07-28 11:51:16 -0400

    [TASK] Calculate the PDF at a data point

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -19,8 +19,29 @@ class MultiNormal():
         if n < 2:
             raise ValueError('data must contain multiple data points')
 
-
         self.mean = np.mean(data, axis=1).reshape(d, 1)
         dev = data - self.mean
 
         self.cov = np.matmul(dev, dev.T) / (n - 1)
+
+    def pdf(self, x):
+        """Calculates the PDF at a data point
+        @x: is np.ndarray shape(d, 1) data point whose PDF should be calced
+            @d: number of dimensions of the multinomial
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError('x must be a numpy.ndarray')
+        shape = x.shape
+        d = self.cov.shape[0]
+        if len(shape) != 2:
+            raise ValueError("x mush have the shape ({}, 1)".format(d))
+
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+
+        dev = x - self.mean
+
+        const = 1 / np.sqrt(((2 * np.pi) ** d) * det)
+
+        pdf = const * np.exp(np.matmul(np.matmul(-dev.T, inv), dev / 2))
+        return pdf.reshape(-1)[0]
