599a8bf91dd8197165cb5aa595520aa50b7deeac
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-07-29 19:18:09 -0400

    [TASK] Make diagonal equal to 0

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -18,8 +18,11 @@ def P_init(X, perplexity):
         @H: the Shannon entropy for perplexity perplexity
     """
     n, d = X.shape
+
     sum_X = np.sum(np.square(X), 1)
     D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+    np.fill_diagonal(D, 0)
+
     P = np.zeros([n, n])
     betas = np.ones([n, 1])
     H = np.log2(perplexity)
