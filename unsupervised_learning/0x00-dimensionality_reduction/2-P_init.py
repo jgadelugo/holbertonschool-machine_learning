90660ba21480a4e8557634a115857cf8f2b29e13
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-07-29 10:23:05 -0400

    [TASK] Init all variables required to calc the P affinities in T-SNE

---
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""initialize all variables required to calculate the P affinities in t-SNE"""
+import numpy as np
+
+
+def P_init(X, perplexity):
+    """
+    initialize all variables required to calculate the P affinities in t-SNE
+    @X: np.ndarray shape(n, d) containing the dataset
+        @n: number of data points
+        @d: number of dimensions each point
+    @perplexity: perplexity that all Gaussian distributions should have
+    Return: (D, P, betas, H)
+        @D: np.ndarray shape(n, n) calculates the pairwise distance
+        @p: np.ndarray shape(n, n) init to all 0's that will contain the P
+        affinities
+        @betas: np.ndarray shape(n, 1) init all 1's all of the beta values
+        @H: the Shannon entropy for perplexity perplexity
+    """
+    n, d = X.shape
+    sum_X = np.sum(np.square(X), 1)
+    D = (np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X))
+    P = np.zeros([n, n], dtype='float64')
+    betas = np.ones([n, 1], dtype='float64')
+    H = np.log2(perplexity)
+    return (D, P, betas, H)
