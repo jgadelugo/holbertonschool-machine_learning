e31fdd08112f7bc12672195188b903c2c7b56740
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-07-29 10:18:54 -0400

    [TASK] Calculate the shannon entropy and P affinities relative to a data point

---
+++ unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""calculates the Shannon entropy and P affinities relative to a data point"""
+import numpy as np
+
+
+def HP(Di, beta):
+    """
+    calculates the Shannon entropy and P affinities relative to a data point
+    @Di: np.ndarray of shape(n -1,) containing the pairwise distance between
+    a data point and all other points except itself
+        @n: the number of data points
+    beta: beta value for the Gaussian distribution
+    Return: (Hi, Pi)
+        @Hi: Shannon entropy of the points
+        @Pi: np.ndarray shape (n - 1), the P affinities of the points
+    """
+    Pi = np.exp(-Di.copy() * beta)
+
+    sumP = np.sum(Pi)
+    # Hi = np.log(sumP) + beta * np.sum(Di * Pi) / sumP
+
+    Pi = Pi / sumP
+    Hi = -np.sum(Pi * np.log2(Pi))
+    return Hi, Pi
