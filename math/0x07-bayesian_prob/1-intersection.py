18b7935310506d3a4cd6a66273aca99dab9f0e2e
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-07-31 20:12:44 -0400

    [TASK] Calculate the intersection

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -13,18 +13,6 @@ def likelihood(x, n, P):
     Return: 1D np.ndarray - the likelihood of obtaining the data, x and n
     for each probability in P, respectively
     """
-    if not isinstance(n, int) or n < 1:
-        raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0:
-        err_msg = "x must be an integer that is greater than or equal to 0"
-        raise ValueError(err_msg)
-    if x > n:
-        raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-    if np.any(P < 0) or np.any(P > 1):
-        raise ValueError("All values in P must be in the range [0, 1]")
-
     ftl = np.math.factorial
     c = ftl(n) / (ftl(x) * ftl(n - x))
 
@@ -44,6 +32,17 @@ def intersection(x, n, P, Pr):
     Return: 1D np.ndarray - intersection of obtaining x and n with each
     probability in P, respectively
     """
+    if not isinstance(n, int) or n < 1:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        err_msg = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(err_msg)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
     lh = likelihood(x, n, P)
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
