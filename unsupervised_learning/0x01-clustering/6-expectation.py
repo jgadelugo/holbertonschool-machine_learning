0f49743b42e2684c8d87bf6de4067657a01663be
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-08-10 20:14:52 -0400

    [TASK] Calculates the expectation step in the EM algorithm for GMM

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -11,7 +11,29 @@ def expectation(X, pi, m, S):
     @m: np.ndarray of shape(k, d) centroid means for each cluster
     @S: np.ndarray of shape (k, d, d) covariance matrices for each cluster
     Returns: g, l or None, None on failure
-        @g: np.ndarray shape(k, n) posterior  probs for each data point
+        @g: np.ndarray shape(k, n) posterior probs for each data point
         @l: total log likelihood
     """
-    return None, None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if (not isinstance(pi, np.ndarray) or len(pi.shape) != 1 or
+        not np.isclose(1, np.sum(pi))):
+        return None, None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+        return None, None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+        return None, None
+    d = X.shape[1]
+    k = pi.shape[0]
+    if m.shape[1] != d or S.shape[1] != S.shape[2] or S.shape[1] != d:
+        return None, None
+    if m.shape[0] != S.shape[0] or m.shape[0] != k:
+        return None, None
+    g = []
+    for i in range(k):
+        num = pi[i] * pdf(X, m[i], S[i])
+        g.append(num)
+    g = np.asarray(g)
+    g_sum = np.sum(g, axis=0)
+    l = np.sum(np.log(g_sum))
+    return g / g_sum, l
