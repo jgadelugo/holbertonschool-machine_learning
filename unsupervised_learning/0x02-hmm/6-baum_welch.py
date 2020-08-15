fa2025e15002d7cdb3f490971bd444db3a1e4200
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-08-14 20:47:15 -0400

    [PREP] Prep file

---
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Performs the backward algorithm for a hidden markov model"""
+import numpy as np
+
+
+def baum_welch(Observation, Transition, Emission, Initial, iterations=1000):
+    """Performs the backward algorithm for a hidden markov model
+    @Observation: np.ndarray of shape(T,) index of the observation
+        @T: number of observations
+    @Emission: np.ndarray shape(N, M) emission probability of a specific
+    observation given a hiden state
+        @Emission[i, j]: probability of observing j given the hidden state i
+        @N: number of hidden states
+        @M: number of all possible observations
+    @Transition: 2D np.ndarray shape(N, N) transition probabilities
+        @Transition[i, j] probability of transitioning  from the hidden state
+        i to j
+    @Initial: np.ndarray shape(N, 1) probability of starting in a particular
+    hidden state
+    Return: P, B, or None, None on failure
+        @P: likelihood of the observation given the model
+        @B: np.ndarray shape(N, T) backward path probabilities
+            @B[i, j] probability of generating the future observation from
+            hidden state i at time j
+    """
+    if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
+        return None, None
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return None, None
+    if not isinstance(Transition, np.ndarray) or len(Transition.shape) != 2:
+        return None, None
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return None, None
+    T = Observation.shape[0]
+    N = Emission.shape[0]
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
+        return None, None
+    if Initial.shape[0] != N or Initial.shape[1] != 1:
+        return None, None
+
+    F = np.zeros((N, T))
+
+    for s in range(N):
+        F[s, 0] = Initial[s, 0] * Emission[s, Observation[0]]
+    for t in range(1, T):
+        for s in range(N):
+            alpha = F[:, t - 1]
+            tran = Transition[s]
+            em = Emission[s, Observation[t]]
+            F[s, t] = np.sum(alpha * tran * em)
+    P = np.sum(F[:, -1])
+    return P, F
