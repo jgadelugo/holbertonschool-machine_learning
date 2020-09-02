e9c96b23919a961a37011726ef55623388ebf2f0
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-09-02 14:50:43 -0400

    [TASK] Class represents a cell of a simple RNN

--- supervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Class that represents a cell of a simple RNN"""
+import numpy as np
 
 
 class RNNCell():
@@ -13,10 +14,12 @@ class RNNCell():
             @Wh and bh are for the concatenated hidden state and input data
             @wy and by are for the output
         """
-        pass
-    
+        self.Wh = np.random.normal(size=(h+i, h))
+        self.Wy = np.random.normal(size=(h, o))
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
 
-    def forware(self, h_prev, x_t):
+    def forward(self, h_prev, x_t):
         """performs forward propagation for one time step
         @h_prev: np.ndarray shape(m, h) containing the previous hidden state
             @h_next: next hidden state
@@ -24,7 +27,14 @@ class RNNCell():
         @x_t: np.ndarray of shape(m, i) containing the data input for the cell
             @m: batche size for the data
         Retrun: h_next, y
-            @h_next is the next hidden state
+            @h_next: the next hidden state
             @y: output of the cell
         """
-        pass
+        con = np.concatenate((h_prev.T, x_t.T), axis=0)
+        h_next = np.tanh((np.matmul(con.T, self.Wh)) + self.bh)
+        soft = np.matmul(h_next, self.Wy) + self.by
+        # softmax
+        x_max = np.max(soft, axis=-1, keepdims=True)
+        x_exp = np.exp(soft - x_max)
+        y = x_exp / x_exp.sum(axis=-1, keepdims=True)
+        return h_next, y
