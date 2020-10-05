573274bf8b6ae81d0bd7938496e125fd5ec25fe7
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-10-05 11:30:25 -0400

    [TASK] Loads and preps dataset

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
 """ Class that loads and preps a dataset for ML"""
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
 
 
 class Dataset():
     """ Class that loads and preps a dataset for ML"""
     def __init__(self):
         """constructor
+        * create instance attributes:
         @data_train, which contains the ted_hrlr_translate/pt_to_en
         tf.data.Dataset train split, loaded as_supervided
         @data_valid, which contains the ted_hrlr_translate/pt_to_en
         tf.data.Dataset validate split, loaded as_supervided
-        @tokenizer_pt is the Portuguese tokenizer created from the training set
-        @tokenizer_en is the English tokenizer created from the training set
+        @tokenizer_pt: Portuguese tokenizer created from the training set
+        @tokenizer_en: English tokenizer created from the training set
         """
-        pass
+        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
+                                       with_info=True,
+                                       as_supervised=True)
+        self.data_train = examples['train']
+        self.data_valid = examples['validation']
+
+        tokenizer_pt, tokenizer_en = self.tokenize_dataset(self.data_train)
+
+        self.tokenizer_pt = tokenizer_pt
+        self.tokenizer_en = tokenizer_en
+
     def tokenize_dataset(self, data):
         """Creates sub-word tokenizers for our dataset
         @data: tf.data.Dataset whose examples are formatted as a tuple (pt, en)
@@ -24,4 +37,10 @@ class Dataset():
             @tokenizer_pt: Portugues tokenizer
             @tokenizer_en: English tokenizer
         """
-        pass
+        tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+            (pt.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+
+        tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+            (en.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+
+        return tokenizer_pt, tokenizer_en
