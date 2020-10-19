70dbf348b5ab6209e0b705acacc20f08cd362806
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-10-19 15:36:19 -0400

    Clean up to pass styling

--- supervised_learning/0x0E-time_series/forecast_btc.py
+++ supervised_learning/0x0E-time_series/forecast_btc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" Train and show """
 import tensorflow as tf
 import numpy as np
 import pandas as pd
--- supervised_learning/0x0E-time_series/preprocess_data.py
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
+""" Clean data"""
 import pandas as pd
 
 
 def preprocess():
-    df_price = pd.read_csv("bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv")
+    """ Clean data"""
+    df_p = pd.read_csv("bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv")
 
     # replace NaN with datapoints
-    df_price['Close'].fillna(method='bfill', inplace=True)
-    df_price['High'].fillna(df_price['Close'], inplace=True)
-    df_price['Low'].fillna(df_price['Close'], inplace=True)
-    df_price['Open'].fillna(df_price['Close'], inplace=True)
-    df_price['Volume_(BTC)'].fillna(0, inplace=True)
-    df_price['Volume_(Currency)'].fillna(0, inplace=True)
-    df_price['Weighted_Price'].fillna(0, inplace=True)
-    return df_price
+    df_p['Close'].fillna(method='bfill', inplace=True)
+    df_p['High'].fillna(df_p['Close'], inplace=True)
+    df_p['Low'].fillna(df_p['Close'], inplace=True)
+    df_p['Open'].fillna(df_p['Close'], inplace=True)
+    df_p['Volume_(BTC)'].fillna(0, inplace=True)
+    df_p['Volume_(Currency)'].fillna(0, inplace=True)
+    df_p['Weighted_Price'].fillna(0, inplace=True)
+    return df_p
