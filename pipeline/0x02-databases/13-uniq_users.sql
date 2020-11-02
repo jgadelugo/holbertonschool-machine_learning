b7965fc8a7fa272d7bc7a78716e4eec4f2484128
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-11-02 15:35:12 -0500

    [TASKS] 10-13

---
+++ pipeline/0x02-databases/13-uniq_users.sql
@@ -0,0 +1,12 @@
+-- script that creates a table users
+-- with these attributes
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- If the table already exists, your script should not fail
+-- Your script can be executed on any database
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY(id));
