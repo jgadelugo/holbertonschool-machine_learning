05d8683c250fa7f7c7cd393a80ad32f8cc458ef0
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-11-02 11:35:43 -0500

    [TASKS] Create database, create table, show all rows in table, insert vaue

---
+++ pipeline/0x02-databases/0-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- script that creates the database db_0 in MySQL server
+CREATE DATABASE IF NOT EXISTS db_0;
---
+++ pipeline/0x02-databases/1-first_table.sql
@@ -0,0 +1,4 @@
+-- Script that creates a table called first_table in database MySQL
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT,
+	name VARCHAR(256));
