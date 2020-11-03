abfe25cf20e87b4202192aec7a4f67f4ea09651f
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-11-03 18:25:33 -0500

    [TASK] Create trigger and procedure

---
+++ pipeline/0x02-databases/19-bonus.sql
@@ -0,0 +1,17 @@
+-- script that creates a stored procedure AddBonus
+-- that adds a new correction for a student
+-- resource: https://www.mysqltutorial.org/stored-procedures-parameters.aspx
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus (
+    IN user_id INTEGER,
+    IN project_name VARCHAR(255),
+    IN score INTEGER)
+BEGIN
+    IF NOT EXISTS (SELECT name FROM projects WHERE name=project_name) THEN
+        INSERT INTO projects(name) VALUES (project_name);
+    END IF;
+    INSERT INTO corrections(user_id, project_id, score)
+        VALUES(user_id,(SELECT id FROM projects WHERE name = project_name), new_score);
+END $$
+DELIMITER ;
