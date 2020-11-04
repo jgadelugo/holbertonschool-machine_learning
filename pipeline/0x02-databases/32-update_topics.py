b8be7f8fd16d741b625fb7cd2fa844ce5723e89b
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-11-04 14:22:51 -0500

    [TASK] 19, 32 and 100

--- pipeline/0x02-databases/32-update_topics.py
+++ pipeline/0x02-databases/32-update_topics.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" function that changes all topics of a school document based on the name"""
+"""function that changes all topics of a school document based on the name"""
 
 
 def update_topics(mongo_collection, name, topics):
