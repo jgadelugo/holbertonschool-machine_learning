7ceeacb9de0edf61946b91a07d0840636004fb75
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-11-03 19:36:11 -0500

    [TASK] changes all topics of a school document based on the name

---
+++ pipeline/0x02-databases/32-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" function that changes all topics of a school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    new = {"$set": {"topics": topics}}
+    mongo_collection.update_many({"name": name}, new)
