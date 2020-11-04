1d4e58789bbcdd16671fa3348d3bae62513e4857
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-11-03 19:24:53 -0500

    [TASK] Function inserts a new doc in collection based on kwargs

---
+++ pipeline/0x02-databases/31-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function that inserts a new document in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection based on kwargs"""
+    return mongo_collection.insert_one(kwargs).inserted_id
